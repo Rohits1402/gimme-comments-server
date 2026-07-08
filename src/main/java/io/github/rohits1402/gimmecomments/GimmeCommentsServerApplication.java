6412355c2f4576918025593e6300008d6d08e796
Rohit <rschauhan1402@gmail.com>
2026-07-08 23:35:59 +0530

    Bootstrap Spring Boot Project From Spring Initializer

---
+++ src/main/java/io/github/rohits1402/gimmecomments/GimmeCommentsServerApplication.java
@@ -0,0 +1,13 @@
+package io.github.rohits1402.gimmecomments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class 	GimmeCommentsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GimmeCommentsServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/rohits1402/gimmecomments/GimmeCommentsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.rohits1402.gimmecomments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GimmeCommentsServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
